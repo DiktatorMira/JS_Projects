6b5abdbfb037c9de06fd4336448955f2ae64a1d8
Diktator <dychervony@gmail.com>
2024-03-27 17:52:35 +0200

    По идее изменил дз за 26.03.2024

--- Dz26.03.2024/script.js
+++ Dz26.03.2024/script.js
@@ -1,49 +1,62 @@
 $(function(){
-    $('.but').on('click', async function() {
-        // Получаем данные от пользователя
-        let title = $('.form .inputs[type="text"]').val(), type = $('.form .inputs[name="type"]').val();
-        $('.films').empty(); // Очищаем предыдущие результаты поиска
-        try {
-            const response = await fetch(`http://www.omdbapi.com/?apikey=ваш_ключ_api&s=${title}&type=${type}`); // Запрос к API OMDB
-            const data = await response.json();
-            $('.films').css('display', 'flex'); // Показываем секцию с фильмами
-            if (data.Response === "True") {
-                data.Search.forEach(movie => { // Отображаем найденные фильмы
-                    $('.films').append(`
-                        <div class="plate">
-                            <img class="image" src="${movie.poster_path}">
-                            <div class="desc">
-                                <p class="desctext">Тип: ${type}</p>
-                                <p class="desctext">Название: ${movie.title}</p>
-                                <p class="desctext">Год выпуска: ${movie.release_date}</p>
-                                <input class="but msecond" type="button" value="Подробнее" data-id="${movie.imdbID}">
-                            </div>
-                        </div>
-                    `);
+    // Обработчик события нажатия на кнопку Поиск
+    $('.mfirst').click(function(){
+        // Получаем значения из полей ввода
+        let title = $('.form .inputs[type="text"]').val();
+        let type = $('.form select[name="type"]').val();
+        // Формируем URL для запроса к API OMDB
+        let url = 'http://www.omdbapi.com/?apikey=0069fc3b-de09-4b51-99c8-084cfd9cea9c&type=' + type + '&s=' + encodeURIComponent(title);
+        // Отправляем AJAX-запрос
+        fetch(url).then(response => response.json()).then(data => {
+            // Проверяем, есть ли фильмы в ответе
+            if(data.Response === "True") {
+                // Отображаем фильмы в films plate
+                $('.films').css('display', 'flex');
+                $('.films').empty(); // Очищаем содержимое films plate
+                data.Search.forEach(function(movie){
+                    // Создаем блок для каждого фильма
+                    let movieBlock = $('<div class="plate"></div>');
+                    let image = $('<img class="image">').attr('src', movie.Poster);
+                    let desc = $('<div class="desc"></div>');
+                    let title = $('<p class="desctext"></p>').text('Название: ' + movie.Title);
+                    let year = $('<p class="desctext"></p>').text('Год выпуска: ' + movie.Year);
+                    let detailsButton = $('<input class="but msecond" type="button" value="Детали">');
+                    // Добавляем данные в блок фильма
+                    desc.append(title, year, detailsButton);
+                    movieBlock.append(image, desc);
+                    $('.films').append(movieBlock);
                 });
-            } else $('.films').append(`<p class="desctext">Фильмы не найдены!</p>`);
-        } catch (error) { console.error('Ошибка: ', error); }
+            } else {
+                // Если фильмы не найдены, отображаем сообщение
+                $('.films').css('display', 'flex');
+                $('.films').html('<p>Movie not found!</p>');
+            }
+        })
+        .catch(error => console.log('Error:', error));
     });
-    // Обработчик для кнопки "Подробнее"
-    $(document).on('click', '.msecond', async function() {
-        let imdbID = $(this).data('id'); // Получаем ID фильма
-        try {
-            // Запрос к API OMDB для получения подробной информации о фильме
-            const response = await fetch(`http://www.omdbapi.com/?apikey=ваш_ключ_api&i=${imdbID}`);
-            const movieData = await response.json();
-            // Показываем секцию с подробной информацией о фильме
+    // Обработчик события нажатия на кнопку Детали
+    $(document).on('click', '.msecond', function(){
+        // Получаем название фильма из блока и формируем ссылку
+        let movieTitle = $(this).siblings('.desc').children('p:first').text().replace('Название: ', ''), url = 'http://www.omdbapi.com/?apikey=0069fc3b-de09-4b51-99c8-084cfd9cea9c&t=' + encodeURIComponent(movieTitle);
+        // Отправляем AJAX-запрос
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Отображаем подробную информацию о фильме в mainplate
             $('.mainplate').css('display', 'flex');
-            if (movieData.Response === "True") { // Отображаем информацию о фильме
-                $('.mainplate .image').attr('src', movieData.backdrop_path);
-                $('.mainplate .desctext:eq(0)').text(`Название: ${movieData.title}`);
-                $('.mainplate .desctext:eq(1)').text(`Дата выпуска: ${movieData.release_date}`);
-                $('.mainplate .desctext:eq(2)').text(`Жанр: ${type}`);
-                $('.mainplate .desctext:eq(3)').text(`Страна: ${movieData.original_language}`);
-                $('.mainplate .desctext:eq(4)').text(`Режиссёр: ${movieData.Director}`);
-                $('.mainplate .desctext:eq(5)').text(`Авторы: ${movieData.Writer}`);
-                $('.mainplate .desctext:eq(6)').text(`Актёры: ${movieData.Actors}`);
-                $('.mainplate .desctext:eq(7)').text(`Награды: ${movieData.Awards}`);
-            } else $('.mainplate .desctext:eq(0)').text('Данные о фильме не найдены!');
-        } catch (error) { console.error('Ошибка:', error); }
+            $('.mainplate .desc').empty(); // Очищаем содержимое mainplate
+            $('.mainplate .image').attr('src', data.Poster);
+            $('.mainplate .desc').append(
+                $('<p class="desctext"></p>').text('Название: ' + data.Title),
+                $('<p class="desctext"></p>').text('Дата выпуска: ' + data.Release),
+                $('<p class="desctext"></p>').text('Жанр: ' + data.Genre),
+                $('<p class="desctext"></p>').text('Страна: ' + data.Country),
+                $('<p class="desctext"></p>').text('Режиссёр: ' + data.Director),
+                $('<p class="desctext"></p>').text('Авторы: ' + data.Writer),
+                $('<p class="desctext"></p>').text('Актёры: ' + data.Actors),
+                $('<p class="desctext"></p>').text('Награды: ' + data.Awards)
+            );
+        })
+        .catch(error => console.log('Error:', error));
     });
 });
